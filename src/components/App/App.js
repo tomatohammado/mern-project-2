6ad5dadb66108d71da706264ee6bdc6e3aec142b
Hammad Malik <hmd.node@gmail.com>
2018-01-22 16:59:29 -0500

    Edit App.js to use static assets, use className vs class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,19 +6,19 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <main class='container'>
+        <main className='container'>
           <header>
             <a href='/'>
-              <h1 class='center'>Star Wars Miniatures App</h1>
+              <h1 className='center'>Star Wars Miniatures App</h1>
             </a>
           </header>
 
           {/* {{{body}}} */}
 
         </main>
-        <footer class='center'>
+        <footer className='center'>
           <a href='http://rebelscum.com/'>
-            <img src='/images/rebelscum.gif' border='0' width='100' height='35' alt='Rebelscum image credit' />>
+            <img src='/assets/images/rebelscum.gif' border='0' width='100' height='35' alt='Rebelscum credit' />
           </a>
         </footer>
       </div>
